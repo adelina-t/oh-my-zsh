f3364b1f603f5d849afafe36c013bbf27fc24785
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2018-07-08 19:43:16 +0300

    Added az completion and vs code plugin

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -21,6 +21,7 @@ fpath=($ZSH/functions $ZSH/completions $fpath)
 
 # Load all stock functions (from $fpath files) called below.
 autoload -U compaudit compinit
+autoload -U +X bashcompinit && bashcompinit
 
 # Set ZSH_CUSTOM to the path where your custom config files
 # and plugins exists, or else we will use the default custom/
